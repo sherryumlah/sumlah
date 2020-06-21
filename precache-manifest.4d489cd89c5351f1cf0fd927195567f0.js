4674c3a39fa75f66f8b1e45b89e55cad8acd662f
Sherry L. Umlah <sillylaughter@gmail.com>
2020-06-21 13:28:50 -0500

    Updates

---
+++ precache-manifest.4d489cd89c5351f1cf0fd927195567f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5630c0ab3d8fc07be667e7e581355ba1",
+    "url": "/sherryumlah/sumlah.git/index.html"
+  },
+  {
+    "revision": "3c5a7b57cba506ea059c",
+    "url": "/sherryumlah/sumlah.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09eed34a47f7bed8a13a",
+    "url": "/sherryumlah/sumlah.git/static/js/2.fc08bc91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sherryumlah/sumlah.git/static/js/2.fc08bc91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5a7b57cba506ea059c",
+    "url": "/sherryumlah/sumlah.git/static/js/main.af7365c2.chunk.js"
+  },
+  {
+    "revision": "ebb6c84d18afea439b7b",
+    "url": "/sherryumlah/sumlah.git/static/js/runtime-main.323fb97d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sherryumlah/sumlah.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
